1550898b8daa4c2efe6c35cc23fa7aba8aa93bf2
Vanessa <zdperfectx@gmail.com>
2023-11-14 17:54:05 -0600

    completed post request code for /pastes. added gloabal variable to create id, destructured data to req.body, created newPaste body to be added into previous paste, pushed, and formatted response as json, an object with data property containing details of new /paste

--- src/app.js
+++ src/app.js
@@ -5,6 +5,31 @@ const pastes = require("./data/pastes-data");
 
 
 
+
+
+app.get("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
+
+// POST request to /pastes
+
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { user_id, name, syntax, expiration, exposure, text } = {} } = req.body;
+  const newPaste = {
+    id: ++lastPasteId,
+    user_id,
+    name,
+    syntax,
+    expiration,
+    exposure,
+    text,
+  };
+  pastes.push(newPaste);
+  res.json({ data: newPaste })
+});
+
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
 app.use("/pastes/:pasteId", (req, res, next) => {
@@ -16,17 +41,6 @@ app.use("/pastes/:pasteId", (req, res, next) => {
     next(`Paste id not found: ${pasteId}`);
   }
 });
-
-app.get("/pastes", (req, res) => {
-  res.json({ data: pastes });
-});
-
-// POST request to /pastes
-
-app.post("/pastes", (req, res, next) => {
-
-})
-
 
 // Not found handler
 app.use((request, response, next) => {
