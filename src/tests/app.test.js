713f9606438fb2a0c184aae907178589689fd5cd
Vanessa <zdperfectx@gmail.com>
2023-11-15 13:19:41 -0600

    implemented test body inside of test method. async, created mock expected array. pushed into pastes after clearing array. made api call with supertest simulation,  expect res status to be 200, expect res body data to equal expected array

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -7,4 +7,43 @@ describe("path /pastes", () => {
     beforeEach(() => {
         pastes.splice(0, pastes.length); // Clears out the pastes data
     })
-})
+    describe("GET method", () => {
+        it("returns an array of pastes", async () => {
+            const expected = [
+                {
+                    id: 1,
+                    user_id: 1,
+                    name: "Hello",
+                    syntax: "None",
+                    expiration: 10,
+                    exposure: "private",
+                    text: "Hello World!"
+                  },
+                  {
+                    id: 2,
+                    user_id: 1,
+                    name: "Hello World in Python",
+                    syntax: "Python",
+                    expiration: 24,
+                    exposure: "public",
+                    text: "print(Hello World!)"
+                  },
+                  {
+                    id: 3,
+                    user_id: 2,
+                    name: "String Reverse in JavaScript",
+                    syntax: "Javascript",
+                    expiration: 24,
+                    exposure: "public",
+                    text: "const stringReverse = str => str.split('').reverse().join('');" 
+                }
+            ];
+
+            pastes.push(...expected);
+            const response = await request(app).get("/pastes");
+
+            expect(response.status).toBe(200);
+            expect(response.body.data).toEqual(expected);
+        });
+    });
+});
