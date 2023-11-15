5c2cae51a4c42e564f5a52ae02ee9eef5ca6a1db
Vanessa <zdperfectx@gmail.com>
2023-11-15 12:34:59 -0600

    implementing jest and supertest to test out API and ensure api returns correct results. requires supertest, pastesm and app.

---
+++ src/tests/app.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const pastes = require("../src/data/pastes-data");
+const app = require("../src/app");
+const { describe } = require("yargs");
+
+describe("path /pastes", () => {
+    // Add tests here
+})
