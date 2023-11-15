d79bdcb6d1bdcd48e2f6c5fccef5e2b62bc509d2
Vanessa <zdperfectx@gmail.com>
2023-11-15 16:01:56 -0600

    added middleware function for validation and error handling and removed validation from post to keep single responsibility principle for post route

--- src/app.js
+++ src/app.js
@@ -5,21 +5,27 @@ const pastes = require("./data/pastes-data");
 // use app.use express.json to parse json data and add a body property to the request (req.body)
 app.use(express.json());
 
-
-
-
-
 app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
+
+// New middleware function to validate request body for post request
+
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next(); // call next without an error message if the result exists
+  }
+  next("A 'text' property is required.");
+}
 
 // POST request to /pastes
 
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.post("/pastes", (req, res, next) => {
-  const { data: { user_id, name, syntax, expiration, exposure, text } = {} } = req.body;
-  if (text) { 
+app.post("/pastes", bodyHasTextProperty, (req, res, next) => {
+  const { data: { user_id, name, syntax, expiration, exposure, text } = {} } =
+    req.body;
   const newPaste = {
     id: ++lastPasteId,
     user_id,
@@ -30,10 +36,7 @@ app.post("/pastes", (req, res, next) => {
     text,
   };
   pastes.push(newPaste);
-  res.status(201).json({ data: newPaste })
-  } else {
-    res.sendStatus(400);
-  }
+  res.status(201).json({ data: newPaste });
 });
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
